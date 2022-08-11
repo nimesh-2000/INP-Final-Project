40ca5f4692665e9a572e366ffa9d54e2aa931152
nimesh-2000 <nimeshdenuwan2000@gmail.com>
2022-08-11 22:21:20 +0530

    complete chat room controlle

--- src/controller/ChatRoomFormController.java
+++ src/controller/ChatRoomFormController.java
@@ -2,15 +2,24 @@ package controller;
 
 import com.jfoenix.controls.JFXTextArea;
 import com.jfoenix.controls.JFXTextField;
+import javafx.application.Platform;
 import javafx.event.ActionEvent;
 import javafx.geometry.NodeOrientation;
+import javafx.geometry.Pos;
+import javafx.scene.Node;
 import javafx.scene.control.Label;
+import javafx.scene.image.Image;
+import javafx.scene.image.ImageView;
+import javafx.scene.input.MouseEvent;
 import javafx.scene.layout.AnchorPane;
+import javafx.scene.layout.HBox;
+import javafx.scene.layout.VBox;
+import javafx.scene.text.Text;
+import javafx.scene.text.TextFlow;
+import javafx.stage.FileChooser;
+import javafx.stage.Stage;
 
-import java.io.BufferedReader;
-import java.io.IOException;
-import java.io.InputStreamReader;
-import java.io.PrintWriter;
+import java.io.*;
 import java.net.Socket;
 
 public class ChatRoomFormController extends Thread{
@@ -18,6 +27,9 @@ public class ChatRoomFormController extends Thread{
     public JFXTextArea txtTextArea;
     public JFXTextField txtTextField;
     public Label lblUser;
+    public VBox vBox;
+    private FileChooser fileChooser;
+    private File filePath;
 
     BufferedReader reader;
     PrintWriter writer;
@@ -68,36 +80,126 @@ public class ChatRoomFormController extends Thread{
     }
 
     @Override
-    public void run(){
+    public void run() {
         try {
             while (true) {
+
 
                 String msg = reader.readLine();
                 String[] tokens = msg.split(" ");
                 String cmd = tokens[0];
-                System.out.println(cmd);
-                // txTextArea.appendText(cmd+"\n");
+
+//                txtTextArea.appendText(cmd+"\n");
                 StringBuilder fullMsg = new StringBuilder();
                 for (int i = 1; i < tokens.length; i++) {
                     fullMsg.append(tokens[i]);
                 }
-                System.out.println(fullMsg);
-                //txTextArea.appendText(cmd+" "+fullMsg+"\n");
-//                if (cmd.equalsIgnoreCase("Client" + ":")) {
-//                    continue;
-//                } else if (fullMsg.toString().equalsIgnoreCase("bye")) {
-//                    break;
-//                }
 
-                System.out.println("cmd="+cmd+"-----"+"UserName"+lblUser.getText());
-                if(!cmd.equalsIgnoreCase(lblUser.getText()+":")){
-                    txtTextArea.appendText(msg + "\n");
+
+                String[] msgToAr = msg.split(" ");
+                String st = "";
+                for (int i = 0; i < msgToAr.length - 1; i++) {
+                    st += msgToAr[i + 1] + " ";
+                }
+//======================================================================
+
+
+
+                Text text = new Text(st);
+                String firstChars = "";
+                if (st.length() > 3) {
+                    firstChars = st.substring(0, 3);
+
                 }
 
+
+                if (firstChars.equalsIgnoreCase("img")) {
+                    //for the Images
+
+                    st = st.substring(3, st.length() - 1);
+
+
+
+                    File file = new File(st);
+                    Image image = new Image(file.toURI().toString());
+
+                    ImageView imageView = new ImageView(image);
+
+                    imageView.setFitHeight(150);
+                    imageView.setFitWidth(200);
+
+
+                    HBox hBox = new HBox(10);
+                    hBox.setAlignment(Pos.BOTTOM_RIGHT);
+
+
+                    if (!cmd.equalsIgnoreCase(lblUser.getText())) {
+
+                        vBox.setAlignment(Pos.TOP_LEFT);
+                        hBox.setAlignment(Pos.CENTER_LEFT);
+
+
+                        Text text1=new Text("  "+cmd+" :");
+                        hBox.getChildren().add(text1);
+                        hBox.getChildren().add(imageView);
+
+                    } else {
+                        hBox.setAlignment(Pos.BOTTOM_RIGHT);
+                        hBox.getChildren().add(imageView);
+                        Text text1=new Text(": Me ");
+                        hBox.getChildren().add(text1);
+
+                    }
+
+                    Platform.runLater(() -> vBox.getChildren().addAll(hBox));
+
+
+                } else {
+                    //For the Text
+//                    text.setFill(Color.WHITE);
+                    //   text.getStyleClass().add("message");
+                    TextFlow tempFlow = new TextFlow();
+
+                    if (!cmd.equalsIgnoreCase(lblUser.getText() + ":")) {
+                        Text txtName = new Text(cmd + " ");
+                        txtName.getStyleClass().add("txtName");
+                        tempFlow.getChildren().add(txtName);
+                    }
+
+                    tempFlow.getChildren().add(text);
+                    tempFlow.setMaxWidth(200); //200
+
+                    TextFlow flow = new TextFlow(tempFlow);
+
+                    HBox hBox = new HBox(12); //12
+
+                    //=================================================
+
+
+                    if (!cmd.equalsIgnoreCase(lblUser.getText() + ":")) {
+
+                        //  tempFlow.getStyleClass().add("tempFlowFlipped");
+                        //  flow.getStyleClass().add("textFlowFlipped");
+                        vBox.setAlignment(Pos.TOP_LEFT);
+                        hBox.setAlignment(Pos.CENTER_LEFT);
+                        hBox.getChildren().add(flow);
+
+                    } else {
+                        // text.setFill(Color.WHITE);
+                        // tempFlow.getStyleClass().add("tempFlow");
+                        // flow.getStyleClass().add("textFlow");
+                        Text text2=new Text(fullMsg+":Me");
+                        TextFlow flow2 = new TextFlow(text2);
+                        hBox.setAlignment(Pos.BOTTOM_RIGHT);
+                        hBox.getChildren().add(flow2);
+                    }
+                    //  hBox.getStyleClass().add("hbox");
+                    Platform.runLater(() -> vBox.getChildren().addAll(hBox));
+                }
+
+
             }
-//            reader.close();
-//            writer.close();
-//            socket.close();
+
         } catch (Exception e) {
             e.printStackTrace();
         }
@@ -141,16 +243,14 @@ public class ChatRoomFormController extends Thread{
 
 
 
-    public void send(){
+    public void send() {
         String msg = txtTextField.getText();
         writer.println(lblUser.getText() + ": " + txtTextField.getText());
-        txtTextField.setNodeOrientation(NodeOrientation.LEFT_TO_RIGHT);
-        txtTextArea.appendText("Me: " + msg + "\n");
+
         txtTextField.clear();
-        if(msg.equalsIgnoreCase("BYE")||(msg.equalsIgnoreCase("logout"))) {
+
+        if (msg.equalsIgnoreCase("BYE") || (msg.equalsIgnoreCase("logout"))) {
             System.exit(0);
-//            Stage stage = (Stage) txtTextMsg.getScene().getWindow();
-//            stage.close();
         }
     }
 
@@ -159,5 +259,13 @@ public class ChatRoomFormController extends Thread{
 
     public void txtSendMessageOnAction(ActionEvent actionEvent) {
         send();
+    }
+
+    public void imageSendOnMouseClicked(MouseEvent mouseEvent) {
+        Stage stage = (Stage) ((Node) mouseEvent.getSource()).getScene().getWindow();
+        fileChooser = new FileChooser();
+        fileChooser.setTitle("Open Image");
+        this.filePath = fileChooser.showOpenDialog(stage);
+        writer.println(lblUser.getText() + " " + "img" + filePath.getPath());
     }
 }
--- src/controller/LoginFormController.java
+++ src/controller/LoginFormController.java
@@ -4,6 +4,7 @@ import com.jfoenix.controls.JFXTextField;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Scene;
+import javafx.scene.control.Alert;
 import javafx.scene.layout.AnchorPane;
 import javafx.stage.Stage;
 
@@ -16,14 +17,21 @@ public class LoginFormController {
 
     public void btnLoginOnAction(ActionEvent actionEvent) throws IOException {
         userName=txtUserName.getText();
-        Stage stage = (Stage) txtUserName.getScene().getWindow();
-        stage.close();
-        Stage stage1=new Stage();
-        stage1.setScene(new Scene(FXMLLoader.load(getClass().getResource("../view/ChatRoomForm.fxml"))));
-        stage1.setResizable(false);
-        //primaryStage.getIcons().add(new Image("location"));
-        stage1.setTitle("sample title");
-        stage1.centerOnScreen();
-        stage1.show();
+       if(userName != null) {
+           Stage stage = (Stage) txtUserName.getScene().getWindow();
+           stage.close();
+           Stage stage1 = new Stage();
+           stage1.setScene(new Scene(FXMLLoader.load(getClass().getResource("../view/ChatRoomForm.fxml"))));
+           stage1.setResizable(false);
+           //primaryStage.getIcons().add(new Image("location"));
+           stage1.setTitle("sample title");
+           stage1.centerOnScreen();
+           stage1.show();
+
+
+       }else{
+           new Alert(Alert.AlertType.WARNING,"Login Failed...!").show();
+
+       }
     }
 }
